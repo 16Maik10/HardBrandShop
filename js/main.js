962db616fa9804625cdc1dde82dd60c4235138bd
16Maik10 <MihailBab13@yandex.ru>
2022-01-27 17:05:15 +0300

    Updates in JS: mouseover closings

--- js/main.js
+++ js/main.js
@@ -2,26 +2,54 @@
 
 
 //Функция видимости элементов
-function visibilityOfElements (el){
+function visibilityOfElement (el){
     el.classList.toggle('invisible');
 }
+
+//Функция закрытия элементов
+function closeWindow (el){
+    el.classList.add('invisible');
+}
+
+
 
 //Открытие/закрытие меню корзины
 const cartLogoEl = document.querySelector('.headerTop__cart');
 const cartMenuEl = document.querySelector('.cart-menu');
 
-cartLogoEl.addEventListener('click', () => cartMenuEl.classList.toggle('invisible'));
 
-
-
-//Доп.меню у 'Browse'
+//Открытие/закрытие доп.меню у 'Browse'
+const browseEl = document.querySelector('.browse');
 const browseDownEl = document.querySelector('.browse__down');
-document.querySelector('.search').addEventListener('click', () => browseDownEl.classList.toggle('invisible'));
+browseEl.addEventListener('click', () => visibilityOfElement(browseDownEl));
 
 
 
-//Меню-бургер для адаптива
+//Открытие/закрытие меню-бургера для адаптива
+const burgerMenuEl = document.querySelector('.burger-menu');
 const menuContentEl = document.querySelector('ul.navLinks_burger');
-document.querySelector('.burger-menu').addEventListener('click', () => menuContentEl.classList.toggle('invisible'))
+burgerMenuEl.addEventListener('click', () => visibilityOfElement(menuContentEl));
+
+//Закрытие всех элементов при клике вне области
+document.addEventListener('click', (event) => {
+    if (!(event.path.includes(cartLogoEl) || (event.path.includes(cartMenuEl)))){
+        closeWindow(cartMenuEl);
+    } 
+
+    if (!(event.path.includes(browseEl) || (event.path.includes(browseDownEl)))){
+        closeWindow(browseDownEl);
+    } 
+
+    if (!(event.path.includes(burgerMenuEl) || (event.path.includes(menuContentEl)))){
+        closeWindow(menuContentEl);
+    } 
+
+});
+
+cartLogoEl.addEventListener('click', () => visibilityOfElement(cartMenuEl));
+
+
+
+
 
 
